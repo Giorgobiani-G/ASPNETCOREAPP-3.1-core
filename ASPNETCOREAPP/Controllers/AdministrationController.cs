ba854eafd97ce70b0f6ebebeb13b394046ebdde4
admin <admin@DESKTOP-LILFTDD>
2022-01-02 01:49:27 +0400

    Add&Edit Role

---
+++ ASPNETCOREAPP/Controllers/AdministrationController.cs
@@ -0,0 +1,130 @@
+﻿using ASPNETCOREAPP.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCOREAPP.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> rolemanager;
+        private readonly UserManager<ApplicationUserc> userManager;
+
+        public AdministrationController(RoleManager<IdentityRole> rolemanager, UserManager<ApplicationUserc> userManager)
+        {
+            this.rolemanager = rolemanager;
+            this.userManager = userManager;
+        }
+
+        public IActionResult CreateRole()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // We just need to specify a unique role name to create a new role
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = model.RoleName
+                };
+
+                // Saves the role in the underlying AspNetRoles table
+                IdentityResult result = await rolemanager.CreateAsync(identityRole);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(model);
+        }
+
+
+        public IActionResult ListRoles()
+        {
+            var roles = rolemanager.Roles;
+
+            return View(roles);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var role = await rolemanager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"The Role with {id} cannot be found";
+                return View("NotFound");
+            }
+
+            var model = new EditRoleViewModel
+            {
+                Id = role.Id,
+                RoleName = role.Name
+            };
+
+            foreach (var user in userManager.Users)
+            {
+                if (await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+
+            }
+
+            return View(model);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+
+        {
+            var role = await rolemanager.FindByIdAsync(model.Id);
+
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"The Role with {model.Id} cannot be found";
+                return View("NotFound");
+            }
+            else
+            {
+                role.Name = model.RoleName;
+              var result =  await rolemanager.UpdateAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+
+                }
+            }
+
+            
+                
+            return View(model);
+
+
+        }
+    }
+}
