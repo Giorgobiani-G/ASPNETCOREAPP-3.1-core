a5044cbc1ae16fabbd58fcbc5dfba37c9d96c12a
admin <admin@DESKTOP-LILFTDD>
2022-01-07 23:22:04 +0400

    ManageUserRoles

--- ASPNETCOREAPP/Controllers/AdministrationController.cs
+++ ASPNETCOREAPP/Controllers/AdministrationController.cs
@@ -75,8 +75,8 @@ namespace ASPNETCOREAPP.Controllers
         [HttpGet]
         public async Task<IActionResult> EditUser(string id)
         {
-           var user = await userManager.FindByIdAsync(id);
-            if (user==null)
+            var user = await userManager.FindByIdAsync(id);
+            if (user == null)
             {
                 return View("NotFound");
             }
@@ -162,7 +162,7 @@ namespace ASPNETCOREAPP.Controllers
         }
 
 
-            [HttpGet]
+        [HttpGet]
         [Authorize(Roles = "Admin")]
         public async Task<IActionResult> EditRole(string id)
         {
@@ -350,6 +350,70 @@ namespace ASPNETCOREAPP.Controllers
                     return View("Error");
                 }
             }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ManageRoles(string userId)
+        {
+            ViewBag.userId = userId;
+
+            var user = await userManager.FindByIdAsync(userId);
+
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User with Id = {userId} cannot be found";
+                return View("NotFound");
+
+            }
+
+            var model = new List<RoleUsersViewModel>();
+
+            foreach (var role in rolemanager.Roles)
+            {
+                var userRolesViewModel = new RoleUsersViewModel
+                {
+                    RoleId = role.Id,
+                    RoleName = role.Name
+                };
+
+                if (await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    userRolesViewModel.IsSelected = true;
+                }
+                else
+                {
+                    userRolesViewModel.IsSelected = false;
+                }
+
+                model.Add(userRolesViewModel);
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ManageRoles(List<RoleUsersViewModel> model, string userId)
+        {
+            var user = await userManager.FindByIdAsync(userId);
+
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User with Id = {userId} cannot be found";
+                return View("NotFound");
+            }
+
+            var roles = await userManager.GetRolesAsync(user);
+            var result = await userManager.RemoveFromRolesAsync(user, roles);
+            result = await userManager.AddToRolesAsync(user, model.Where(x => x.IsSelected).Select(y => y.RoleName));
+
+
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Cannot add selected roles to user");
+                return View(model);
+            }
+
+            return RedirectToAction("EditUser", new { Id = userId });
+
         }
     }
 }
---
+++ ASPNETCOREAPP/Models/RoleUsersViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCOREAPP.Models
+{
+    public class RoleUsersViewModel
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
